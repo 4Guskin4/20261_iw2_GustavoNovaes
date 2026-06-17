3116effdff8de502187b7ef7ca399c8b6705b1ef
4Guskin4 <gusmid2010@gmail.com>
2026-06-17 11:38:41 -0300

    Add files via upload

---
+++ apaga.php
@@ -0,0 +1,8 @@
+<?php  
+include 'conecta.php';
+
+$id = $_POST['id'];
+$sql = "DELETE FROM tb_camiseta WHERE id = :id";
+$stmt = $pdo->prepare($sql);
+$stmt->execute([':id' => $id]);
+?>
---
+++ atualiza.php
@@ -0,0 +1,24 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['acao']) && $_POST['acao'] == 'editar') {
+    
+    include 'conecta.php';
+
+    $id = $_POST['id'];
+    $cor = $_POST['cor'];
+    $tamanho = $_POST['tamanho'];
+
+    try {
+        $sql = "UPDATE tb_camiseta SET cor = :cor, tamanho = :tamanho WHERE id = :id";
+        $stmt = $pdo->prepare($sql);
+        
+        if ($stmt->execute([':cor' => $cor, ':tamanho' => $tamanho, ':id' => $id])) {
+            echo "Sucesso";
+        } else {
+            echo "Erro ao atualizar no banco de dados.";
+        }
+    } catch (PDOException $e) {
+        echo "Erro: " . $e->getMessage();
+    }
+    exit;
+}
+?>
